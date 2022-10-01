0dda07d5701b3327c86c6b0ada49b88071c834a9
artsung410 <61309735+artsung410@users.noreply.github.com>
2022-10-01 15:10:25 +0900

    [ADD]Player Move, JUMP, Bullet, Halth, Health UI

---
+++ Assets/Scripts/BasicSpawner.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+using UnityEngine.SceneManagement;
+
+public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [SerializeField]
+    private NetworkRunner networkRunner = null; // NetworkRunner는 PhotonView와 동일하며, 동적으로 할당된다.
+
+    [SerializeField]
+    private NetworkPrefabRef playerPrefab; // Join 했을때 나타나는 플레이어 프리팹
+
+    [SerializeField]
+    private GameMode gameMode; // 서버 or 클라이언트 설정
+
+    private Dictionary<PlayerRef, NetworkObject> playerList = new Dictionary<PlayerRef, NetworkObject>(); // 플레이어의 정보를 저장하기 위한 딕셔너리
+
+    private void Start()
+    {
+        StartGame(GameMode.AutoHostOrClient);
+    }
+
+    async void StartGame(GameMode mode)
+    {
+        networkRunner.ProvideInput = true; // Runner가 입력을 받을수 있도록 함.
+
+        // async await은 대기를 하는것을 의미한다.
+        await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "Fusion Room",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        // 플레이어 위치 설정
+        Vector3 spawnPosition = Vector3.up * 2;
+        NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, spawnPosition, Quaternion.identity, player);
+
+        playerList.Add(player, networkPlayerObject);
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        // Find and remove the players avatar
+        if (playerList.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            playerList.Remove(player);
+        }
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        var data = new NetworkInputData();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            data.movementInput += Vector3.forward;
+            Debug.Log("W");
+        } 
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            data.movementInput += Vector3.back;
+            Debug.Log("S");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            data.movementInput += Vector3.left;
+            Debug.Log("A");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            data.movementInput += Vector3.right;
+            Debug.Log("D");
+        }
+
+        data.buttons.Set(InputButtons.JUMP, Input.GetKey(KeyCode.Space));
+        data.buttons.Set(InputButtons.FIRE, Input.GetKey(KeyCode.Mouse0));
+
+        input.Set(data);
+    }
+
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+
+    }
+
+
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+
+    }
+
+
+
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {
+
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using UnityEngine.UI;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField]
+    private NetworkCharacterControllerPrototype networkCharacterController = null;
+
+    [SerializeField]
+    private Bullet bulletPrefab;
+
+    [SerializeField]
+    private Image hpBar = null;
+
+    [SerializeField]
+    private MeshRenderer meshRenderer = null;
+
+    [SerializeField]
+    private float moveSpeed = 15f;
+
+    [SerializeField]
+    private int maxHp = 100;
+
+    // OnChanaged = 함수에 리스너를 등록하면, 어트리뷰트로 지정한 변수가 바뀔때 마다 리스너를 호출하게할수 있음
+    // ex) OnHpChanged(리스너) 플레이어
+    [Networked(OnChanged = nameof(OnHpChanged))]
+    public int Hp { get; set; }
+
+    [Networked]
+    public NetworkButtons ButtonsPrevious { get; set; }
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            Hp = maxHp;
+        }
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            NetworkButtons buttons = data.buttons;
+            var pressed = buttons.GetPressed(ButtonsPrevious);
+            ButtonsPrevious = buttons;
+
+            Vector3 moveVector = data.movementInput.normalized;
+            networkCharacterController.Move(moveSpeed * moveVector * Runner.DeltaTime);
+
+            if (pressed.IsSet(InputButtons.JUMP))
+            {
+                networkCharacterController.Jump();
+            }
+
+            if (pressed.IsSet(InputButtons.FIRE))
+            {
+                Runner.Spawn(
+                    bulletPrefab,
+                    transform.position + transform.TransformDirection(Vector3.forward),
+                    Quaternion.LookRotation(transform.TransformDirection(Vector3.forward)),
+                    Object.InputAuthority);
+            }
+        }
+
+        if (Hp <= 0 || networkCharacterController.transform.position.y <= -5f)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        networkCharacterController.transform.position = Vector3.up * 2;
+        Hp = maxHp;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        if (Object.HasStateAuthority)
+        {
+            Hp -= damage;
+        }
+    }
+
+    private static void OnHpChanged(Changed<PlayerController> changed)
+    {
+        changed.Behaviour.hpBar.fillAmount = (float)changed.Behaviour.Hp / changed.Behaviour.maxHp;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ChangeColor_RPC(Color.red);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangeColor_RPC(Color.green);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ChangeColor_RPC(Color.blue);
+        }
+    }
+    
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    private void ChangeColor_RPC(Color newColor)
+    {
+        meshRenderer.material.color = newColor;
+    }
+
+    //[Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    //public void RPC_SendMessage(string message, RpcInfo info = default)
+    //{
+    //    if (info.Source == Runner.Simulation.LocalPlayer)
+    //        message = $"You: {message}\n";
+    //    else
+    //        message = $"Other: {message}\n";
+
+    //    ChatSignal(message);
+    //}
+}
